1707c37c6c172ea43e64904cf0c91dfdbbe8b7b6
jsantore <jsantore@bridgew.edu>
2020-11-03 20:40:33 -0500

    used current time to create random seed for random location of gold

--- GmeEngineDemo.go
+++ GmeEngineDemo.go
@@ -8,59 +8,60 @@ import (
 	_ "image/png"
 	"log"
 	"math/rand"
+	"time"
 )
 
-const(
-	ScreenWidth = 800
+const (
+	ScreenWidth  = 800
 	ScreenHeight = 700
 )
 
-type Sprite struct{
+type Sprite struct {
 	pict *ebiten.Image
-	xLoc 	int
-	yLoc 	int
-	dx 		int
-	dy 		int
+	xLoc int
+	yLoc int
+	dx   int
+	dy   int
 }
 
-type Game struct{
-	playerSprite Sprite
-	coinSprite Sprite
-	drawOps ebiten.DrawImageOptions
+type Game struct {
+	playerSprite  Sprite
+	coinSprite    Sprite
+	drawOps       ebiten.DrawImageOptions
 	collectedGold bool
 }
 
-func gotGold(player, gold Sprite) bool{
+func gotGold(player, gold Sprite) bool {
 	goldWidth, goldHeight := gold.pict.Size()
 	playerWidth, playerHeight := player.pict.Size()
 	if player.xLoc < gold.xLoc+goldWidth &&
 		player.xLoc+playerWidth > gold.xLoc &&
 		player.yLoc < gold.yLoc+goldHeight &&
-		player.yLoc+playerHeight > gold.yLoc{
+		player.yLoc+playerHeight > gold.yLoc {
 		return true
 	}
 	return false
 }
 
 func (game *Game) Update() error {
-	if inpututil.IsKeyJustPressed(ebiten.KeyLeft){
-		game.playerSprite.dx=-3
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyRight){
+	if inpututil.IsKeyJustPressed(ebiten.KeyLeft) {
+		game.playerSprite.dx = -3
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
 		game.playerSprite.dx = 3
-	} else if inpututil.IsKeyJustReleased(ebiten.KeyRight) || inpututil.IsKeyJustReleased(ebiten.KeyLeft){
+	} else if inpututil.IsKeyJustReleased(ebiten.KeyRight) || inpututil.IsKeyJustReleased(ebiten.KeyLeft) {
 		game.playerSprite.dx = 0
 	}
 
-	if inpututil.IsKeyJustPressed(ebiten.KeyUp){
-		game.playerSprite.dy= -3
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyDown){
+	if inpututil.IsKeyJustPressed(ebiten.KeyUp) {
+		game.playerSprite.dy = -3
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyDown) {
 		game.playerSprite.dy = 3
-	} else if inpututil.IsKeyJustReleased(ebiten.KeyUp) || inpututil.IsKeyJustReleased(ebiten.KeyDown){
+	} else if inpututil.IsKeyJustReleased(ebiten.KeyUp) || inpututil.IsKeyJustReleased(ebiten.KeyDown) {
 		game.playerSprite.dy = 0
 	}
 	game.playerSprite.yLoc += game.playerSprite.dy
 	game.playerSprite.xLoc += game.playerSprite.dx
-	if game.collectedGold ==false{
+	if game.collectedGold == false {
 		game.collectedGold = gotGold(game.playerSprite, game.coinSprite)
 	}
 	return nil
@@ -82,29 +83,29 @@ func (g Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight
 	return ScreenWidth, ScreenHeight
 }
 
-func main(){
+func main() {
 	ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
 	ebiten.SetWindowTitle("Comp510 First Graphics")
 	gameObject := Game{}
 	loadImage(&gameObject)
-	gameObject.playerSprite.yLoc = ScreenHeight/2
-	width, height:= gameObject.coinSprite.pict.Size()
-	//rand.Seed(982982439842398)
-	gameObject.coinSprite.xLoc = rand.Intn(ScreenWidth-width)
-	gameObject.coinSprite.yLoc = rand.Intn(ScreenHeight-height)
-	if err:= ebiten.RunGame(&gameObject); err!= nil{
+	gameObject.playerSprite.yLoc = ScreenHeight / 2
+	width, height := gameObject.coinSprite.pict.Size()
+	rand.Seed(int64(time.Now().Second()))
+	gameObject.coinSprite.xLoc = rand.Intn(ScreenWidth - width)
+	gameObject.coinSprite.yLoc = rand.Intn(ScreenHeight - height)
+	if err := ebiten.RunGame(&gameObject); err != nil {
 		log.Fatal("Oh no! something terrible happened", err)
 	}
 }
 
 func loadImage(game *Game) {
-	pict, _, err:= ebitenutil.NewImageFromFile("galleon.png")
-	if err != nil{
+	pict, _, err := ebitenutil.NewImageFromFile("galleon.png")
+	if err != nil {
 		log.Fatal("failed to load image", err)
 	}
 	game.playerSprite.pict = pict
-	coins, _, err:= ebitenutil.NewImageFromFile("gold-coins-large.png")
-	if err != nil{
+	coins, _, err := ebitenutil.NewImageFromFile("gold-coins-large.png")
+	if err != nil {
 		log.Fatal("failed to load image", err)
 	}
 	game.coinSprite.pict = coins
